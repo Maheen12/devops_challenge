c8f4992959cad256d1295434d8e1a98418562953
Maheen Sajid <maheenmuneeb12@gmail.com>
2020-10-16 02:15:17 +0200

    Fix docker deploy config

--- webhook/deployment-tutorial/deploy.sh
+++ webhook/deployment-tutorial/deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
+#!/bin/bash
 cd ~
-cd docker-deploy
-git pull
-
-#cd /home/ubuntu/docker-ngnixn
-#sudo docker stop $(sudo docker ps -a -q)
-#sudo docker rm $(sudo docker ps -a -q)
-#sudo docker build -t myapp-nginx .
-#sudo docker run -d -it -p 8080:80 myapp-nginx
-#sudo docker run --name jenkins -p 8080:8080 jenkins:latest
+git clone https://github.com/Maheen12/devops_challenge.git
+cd devops_challenge/webapp
+sudo docker stop $(sudo docker ps -a -q)
+sudo docker rm $(sudo docker ps -a -q)
+sudo docker build -t myapp-nginx .
+sudo docker run -d -it -p 8080:80 myapp-nginx
